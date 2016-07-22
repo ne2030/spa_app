ffa5473a513ed1ef97a07e019fc50509ac8da484
배경훈 <baegyeonghun@baegyeonghun-ui-MacBook-Pro.local>
2016-07-22 13:00:23 +0900

    Revert "express 분리 -> error 발생 (cannot read property 'pathname' of undefined)"
    This reverts commit 2fd428a98485bf9b6b41f47e5d63704957828dd3.

--- server.js
+++ server.js
@@ -3,17 +3,49 @@
     'use strict';
 
     // node_module dependencies
-    var path = require('path');
+    var express = require('express'),
+        path = require('path'),
+        async = require('async'),
+        bodyParser = require('body-parser'),
+        swig = require('swig'),
+        consolidate = require('consolidate');
+
+    var app = express();
 
     // custom js module
     var config = require('./config/config'),
         sequelize = require('./config/sequelize');
 
-        require('./config/express')();
+    app.engine('html', consolidate.swig);
+    app.set('view engine', 'html');
+    app.set('views', __dirname + './app/views');
 
-     /**
-      *   Sequelize setting
-      */
+    // file middleware
+    app.use(bodyParser.json());
+    app.use(express.static(path.join(__dirname, './public')));
+    app.use(express.static(path.join(__dirname, './bower_components')));
+    app.use(express.static(path.join(__dirname, './app')));
+    app.use(express.static(path.join(__dirname, './app/views')));
+
+
+    // express
+
+    app.get('/', function(req, res) {
+        res.render('index');
+    });
+
+    var router = express.Router();
+
+    // Globbing route files
+    config.getGlobbedFiles('./app/routes/*.js').forEach(function(routePath) {
+        console.log(routePath);
+		var _router = require(path.resolve(routePath))(router);
+		app.use(_router);
+	});
+
+    /**
+     *   Sequelize setting
+     */
 
     // var seed = function () {
     //     sequelize.sequelize.sync({
@@ -22,5 +54,9 @@
     //         require('./config/seed')(sequelize);
     //     });
     // }();
+
+    app.listen(8080, function() {
+        console.log('server is running at localhost:8080');
+    });
 
 })();
