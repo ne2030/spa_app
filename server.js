bc8bb640d2061783f248d2e84773ac4279ee19f6
경훈 <gyeonghun@gyeonghunui-MacBook-Pro.local>
2016-12-12 17:30:07 +0900

    seed 수

--- server.js
+++ server.js
@@ -5,29 +5,31 @@ const webpack = require('webpack')
     , webpackDevServer =  require('webpack-dev-server');
 
 const app = require('./config/express')();
+const sequelize = require('./config/sequelize');
 
 const port = 80;
 const devPort = 3002;
 
 require('./config/passport')();
 
+
+
 /**
  *   Sequelize setting
  */
-// let createSeed = function(){
-//   let sequelize = require('./config/sequelize');
+// (() => {
 //   sequelize.sequelize.sync({
 //     force: true
 //   }).then(function () {
 //     require('./config/seed.js')(sequelize);
 //   });
-// };
+// })();
 
 
 if(process.env.NODE_ENV == 'development') {
   console.log('Server is running on development');
 
-// createSeed();
+
 
   const config = require('./webpack.dev.config');
   let compiler = webpack(config);
